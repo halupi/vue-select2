30bb6525a9bc79af68dee443987c0db567a2c391
Jorge Ramon <jorg3.r4mon@gmail.com>
2015-07-26 18:10:59 -0500

    Updating directive

--- dist/vue-select2.js
+++ dist/vue-select2.js
@@ -4,21 +4,17 @@
 	var vueSelect2 = {};
 
 	vueSelect2.install = function(Vue) {
-		var directive = {
-			update: function(value) {
-				var _this = this;
-				switch( _this.arg ) {
-					case "options": $(_this.el).select2({ data: value });
-						break;
+		Vue.directive("select2", function(value) {
+			var vm = this.vm; 
+			var params = value.options || {}; 
+			params.data = value.data || [];
 
-					case "value":
-						$(_this.el).off("select2:select").on("select2:select", function(e) { _this.vm.$data[_this.expression] = $(this).val() });
-						break;
-				}
-			}
-		}
-
-		Vue.directive("select2", directive);
+			$(this.el).select2( params );
+			$(this.el).off("select2:select")
+				.on("select2:select", function(e) {
+					vm.$data[ value.value ] = params.multiple ? $(this).select2('data') : $(this).select2('data')[0];
+				});
+		});
 	}
 
 	if (typeof exports === "object") module.exports = vueSelect2
